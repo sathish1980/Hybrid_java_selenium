26188172febf16aa034d04fa8a6d8f7cf040a0d7
sathish1980 <kumar.sathish189@gmail.com>
2021-12-04 13:43:14 +0530

    main class adedd

---
+++ src/test/java/first/GitTutorial/Mainclass.java
@@ -0,0 +1,15 @@
+package first.GitTutorial;
+
+public class Mainclass {
+	
+	public void add()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
